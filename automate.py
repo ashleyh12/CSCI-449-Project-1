8bb35fc9509597852f5e1dce40549f6d3265e55c
Metbcy <amirbredy1@gmail.com>
2023-10-04 12:19:42 -0400

    Rewriting Traceroute Code

--- automate.py
+++ automate.py
@@ -1,16 +1,17 @@
-from scapy.all import *
-ipaddr = "1.2.3.4"
-for i in range(1, 28):
-  pkt = IP(dst=ipaddr, ttl=i) / UDP(dport=33434)
-  # Send the packet and get a reply
-  reply = sr1(pkt, verbose=0)
-  if reply is None:
-    # No reply
-    break
-  elif reply.type == 3:
-    # We've reached our destination
-    print "Done!", reply.src
-    break
-  else:
-    # We're in the middle somewhere
-    print "%d hops away: " % i , reply.src
+import subprocess
+
+def traceroute(hostname):
+    '''
+    This will take in our hostname or IP address and return a list of the IP hops
+    '''
+    traceroute = subprocess.Popen(["traceroute",hostname],stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    ipList = []
+    for line in iter(traceroute.stdout.readline,b""):
+        line = line.decode("UTF-8")
+        IP = line.split("  ")
+        if len(IP)>1:
+            IP = IP[1].split("(")
+            if len(IP)>1:
+                IP = IP[1].split(")")
+                ipList.append(IP[0])
+    return ipList
