904752b3286e8dcbc9eaa14cc354de56d8ed0252
ashley <90946757+ashleyh12@users.noreply.github.com>
2023-10-17 20:48:28 -0400

    Update NetworkX.py
    Placed the NetworkX code from automate.py into a separate file (NetworkX.py)

--- NetworkX.py
+++ NetworkX.py
@@ -2,3 +2,8 @@
 Networkx function should go here.
 
 '''
+
+# Draw and display the network graph
+pos = nx.spring_layout(G)
+nx.draw(G, pos, with_labels=True, node_size=2000, node_color="skyblue", font_size=10, font_color="black")
+plt.show()
