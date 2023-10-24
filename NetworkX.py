65737fdba8e045f2464caae9ecb9cb4f9df7a079
Metbcy <amirbredy1@gmail.com>
2023-10-24 13:13:05 -0400

    fixing the graphing function

--- NetworkX.py
+++ NetworkX.py
@@ -1,42 +1,35 @@
 '''
-Networkx function should go here.
+Current working networkingX code.
 
 '''
 import json
 import networkx as nx
+import matplotlib.pyplot as plt
 
-# Create an empty directed graph
-G = nx.DiGraph()
+def create_network_topology(json_file):
+    with open(json_file, 'r') as f:
+        data = json.load(f)
 
-# Open the file for reading
-with open("traceroute_results.json", "r") as f:
-    json_data = ""
-    for line in f:
-        json_data += line.strip()
+    G = nx.Graph()
 
-        # Check if the line contains a complete JSON object
-        if json_data and json_data[-1] == "}":
-            try:
-                data = json.loads(json_data)
+    for entry in data['output']:
+        target_ip = entry['target_ip']
+        G.add_node(target_ip)
 
-                if "target_ip" in data and "traceroute" in data:
-                    target_ip = data["target_ip"]
-                    traceroute = data["traceroute"]
+        for hop in entry['traceroute']:
+            hop_ip = hop['ip']
+            G.add_node(hop_ip)
+            G.add_edge(target_ip, hop_ip)
 
-                    # Add nodes and edges to the graph based on the traceroute data
-                    previous_hop = None
-                    for hop in traceroute:
-                        ip = hop.get("ip")
-                        if ip:
-                            G.add_node(ip)
-                            if previous_hop:
-                                G.add_edge(previous_hop, ip)
-                            previous_hop = ip
+    return G
 
-                json_data = ""
+def main():
+    json_file = 'traceroute_results.json'  
+    G = create_network_topology(json_file)
 
-            except json.JSONDecodeError:
-                json_data = ""
+    pos = nx.spring_layout(G, seed=42)
+    nx.draw(G, pos, with_labels=True, node_size=900, font_size=8, node_color='skyblue')
+    plt.show()
 
-
-
+if __name__ == '__main__':
+    main()
